9736aa8dc472649dc977c8f53b1a2102218ff59a
AndréStein <andresteindasilva@gmail.com>
2024-04-02 17:56:38 -0300

    teste

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from './Components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
